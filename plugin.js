5915aac427f8f9bdcd5ee6353a175457fd1a1eef
Andika Tulus Pangestu <62005221+andikatuluspangestu@users.noreply.github.com>
2021-08-21 13:19:28 +0700

    Add files via upload

---
+++ plugin.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('#data_table').DataTable({
+        'paging'      : true,
+        'lengthChange': false,
+        'searching'   : false,
+        'ordering'    : false,
+        'info'        : false,
+        'autoWidth'   : true
+    });
+});
